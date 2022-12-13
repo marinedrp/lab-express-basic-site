2160ed1b018ec0cfd2e4addc6fdf4f50905a5a35
marinedrp <marine.drp@outlook.com>
2022-12-13 18:52:24 +0100

    remove a picture and the instructions

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-/*Instructions
-Your Website should have (at least) 3 pages:
-
-A Home page, that welcomes the visitor and links to the other pages on the website.
-An About page, that shares some basic information about the person.
-A Works page, where you showcase some of the work of the person you chose.
-Bonus: A Photo Gallery page, displaying some pictures of the person/their work.
-You should go through the following steps:
-
-Run npm init -y to create a package.json file.
-Run npm install express to install the Express package.
-Create a views/ directory that will hold your .html files.
-Create a public/ directory that will hold other static files, such as your CSS stylesheets, scripts and images.
-Build your Express app in app.js.
-Polish your HTML, stylesheets and scripts.
-*/
-
-
 const express = require("express");
 
 const app = express();
